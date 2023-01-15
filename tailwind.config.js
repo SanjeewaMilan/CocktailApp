f71e81b802944202c81199cc2e8b0cf9e463e1d7
Sanjeewa Milan <sanjeewamj@gmail.com>
2023-01-16 03:12:04 +0530

    index & app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00264f",
+        secondary: "#00c5f0",
+      },
+    },
+  },
+  plugins: [],
+};
